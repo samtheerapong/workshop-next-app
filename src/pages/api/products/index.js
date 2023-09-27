acf26e597b798972f9eaaa8faac7a7568ccc0aa7
samtheerapong <theerapong.khan@gmail.com>
2023-09-27 13:45:17 +0700

    first init

---
+++ src/pages/api/products/index.js
@@ -0,0 +1,20 @@
+import { create, update, list, remove, read } from "@/server/controllers/products"
+
+export default (req, res) => {
+    // res.send('hello world')
+    // console.log(req.method)
+
+    if (req.method === 'GET') {
+        list(req, res)
+    } else if (req.method === 'POST') {
+        create(req, res)
+    } else if (req.method === 'PUT') {
+        update(req, res)
+    } else if (req.method === 'DELETE') {
+        remove(req, res)
+    // } else if (req.method === 'PATCH') {
+    //     req.send('PATCH')
+    } else {
+        req.status(400).send('Bad Request')
+    }
+}
---
+++ src/server/controllers/products.js
@@ -0,0 +1,49 @@
+export const create = async (req, res) => {
+    try {
+        console.log('Crontroller Create')
+        res.send('Crontroller Create')
+    } catch {
+        console.log(err)
+        res.status(400).send('Create failed')
+    }
+}
+
+export const list = async (req, res) => {
+    try {
+        console.log('Crontroller List')
+        res.send('Crontroller List')
+    } catch {
+        console.log(err)
+        res.status(400).send('List failed')
+    }
+}
+
+export const read = async (req, res) => {
+    try {
+        console.log('Crontroller Read')
+        res.send('Crontroller Read')
+    } catch {
+        console.log(err)
+        res.status(400).send('Read failed')
+    }
+}
+
+export const update = async (req, res) => {
+    try {
+        console.log('Crontroller Update')
+        res.send('Crontroller Update')
+    } catch {
+        console.log(err)
+        res.status(400).send('Update failed')
+    }
+}
+
+export const remove = async (req, res) => {
+    try {
+        console.log('Crontroller Remove')
+        res.send('Crontroller Remove')
+    } catch {
+        console.log(err)
+        res.status(400).send('Remove failed')
+    }
+}
